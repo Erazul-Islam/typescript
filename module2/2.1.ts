661fd0610d5e0123dde8d54a62a6539fa0bf73c5
Erazul Islam <erazul89@gmail.com>
2024-05-02 22:25:46 +0600

    added

---
+++ module2/2.1.ts
@@ -0,0 +1,23 @@
+{
+    // type assertion
+
+    let anything : any;
+    anything = 'Next level web Development';
+
+    anything = 222;
+
+    (anything as number)
+
+    const KgtoGram = (value:string | number) : string | number | undefined => {
+        if(typeof value === 'string'){
+            const convertedValue = parseFloat(value)
+            return ` The converted value is ${convertedValue}`
+        }
+        if(typeof value === 'number'){
+            return value * 1000
+        }
+    }
+
+    const result1 = KgtoGram(1000) as number
+    const result2 = KgtoGram("1000")
+}
