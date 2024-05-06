dd0b89d3714d795e8c774fdf98c6f95af7929e42
Erazul Islam <erazul89@gmail.com>
2024-05-06 11:36:10 +0600

    added\

---
+++ module3/3.1.ts
@@ -0,0 +1,30 @@
+{
+
+  // class and object
+
+  class Animal {
+    name: string;
+    species: string;
+    sound: string
+
+    constructor(name: string, species: string, sound: string) {
+      this.name = name;
+      this.species = species;
+      this.sound = sound
+    }
+
+    makeSound() {
+      console.log(`the ${this} says ${this.sound}`)
+    }
+  }
+
+
+  const dog = new Animal("German Shephard", 'dog', 'yeah')
+  const cat = new Animal('Persian', 'cat', 'af')
+
+  cat.makeSound()
+
+
+
+
+}
